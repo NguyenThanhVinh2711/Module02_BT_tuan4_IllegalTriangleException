78dd77f314f80c34e8c5873c0fc44d364db5981b
boomka <vinh99657@gmail.com>
2022-08-24 18:33:26 +0700

    tuan4

---
+++ src/TriangleEdgesException.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TriangleEdgesException extends Exception {
+    // Ham khoi tao
+    public TriangleEdgesException(String str) {
+        super(str);
+    }
+}
+
